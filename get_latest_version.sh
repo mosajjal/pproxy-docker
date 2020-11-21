f93f698c93e5d159d88ad033499d0be9636349f2
Alireza Mosajjal <mosajjal@gmail.com>
2020-11-22 11:14:31 +1300

    auto tag based on version

--- get_latest_version.sh
+++ get_latest_version.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
-curl -s https://pypi.org/pypi/pproxy/json | jq -r ".info.version" | tee latest_version
+output=`curl -s https://pypi.org/pypi/pproxy/json | jq -r ".info.version" | tee latest_version`
+echo "::set-output name=version::$output"
